b54418ac19d2d051ebdaad209eb6eca24cb97bae
AlbinaBezchastnaya <Albina.Bezchastnaya@yandex.ru>
2024-10-07 12:05:05 +1000

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include "SC.h"
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	system("chcp 1251");                           
+	string curName;          //  текущее имя пользователя
+	string curPassWord;      //  текущий пароль пользователя
+	int messNumber = 0;      //  номер текущего сообщения (сообщения нумеруются от нуля и далее в порядке создания)
+	bool persPresence = false;       //  переменная, показывающая существование пользователя с данным именем и паролем
+	bool nameInChatAlready = false;  //  переменная, которая показывает при регистрации есть ли уже в чате пользователь с таким именем
+
+	string contProcNumber = "0";     // переменная, которая отвечает за продолжение программы	
+
+	Chat presChat;
+
+
+	while (true)
+	{
+		while (persPresence == false)  // цикл продолжается пока пользователь не вошел в аккаунт или не зарегистрировался
+		{
+			while (contProcNumber != "1" && contProcNumber != "2" && contProcNumber != "3")  //  защита от некорректного ввода выбора дальнейших действий
+			{
+				cout << "Выберите действие: 1 - войти; 2 - зарегистрироваться; 3 - выход из программы." << endl;
+				getline(cin, contProcNumber);
+			}
+			persPresence = false;
+			if (contProcNumber == "1")
+			{
+				presChat.entrChat(persPresence, curName, curPassWord);      // войти в чат
+				if (presChat.ifEmpty() && persPresence)                     // если в массиве есть сообщения и пользователь вошел в чат
+				{
+					presChat.showMess(curName);
+				}
+				contProcNumber = "0";
+			}
+
+			if (contProcNumber == "2")
+			{
+				presChat.regChat(nameInChatAlready, persPresence, curName, curPassWord);    // зарегистрироваться в чате
+				if (presChat.ifEmpty() && persPresence)                     // если в массиве есть сообщения и пользователь успешно зарегистрировался
+				{
+					presChat.showMess(curName);
+				}
+				contProcNumber = "0";
+			}
+
+			if (contProcNumber == "3") break;  //  выход из цикла
+		}
+		if (contProcNumber == "3") break;      //  выход из программы
+		while (true)                       // цикл продолжается пока пользователь не захочет выйти из профиля или из программы
+		{
+			while (contProcNumber != "1" && contProcNumber != "2" && contProcNumber != "3")  //  защита от некорректного ввода выбора дальнейших действий
+			{
+				cout << "Выберите действие: 1 - отправить сообщение; 2 - удалить сообщение ; 3 - выйти из профиля." << endl;
+				getline(cin, contProcNumber);
+			}
+			if (contProcNumber == "3")     //  если выбран выход из профиля
+			{
+				cout << endl;              
+				persPresence = false;      //  пользователь выходит из профиля
+				contProcNumber = "0";
+				break;                     //  выход из цикла
+			}
+			if (contProcNumber == "2")     //  если выбрано удаление сообщения
+			{
+				presChat.delMess();
+				contProcNumber = "0";
+			}
+
+			if (contProcNumber == "1")     //  если выбрана отправка сообщения
+			{
+				presChat.sendMess(curName);
+				contProcNumber = "0";
+			}
+			if (contProcNumber == "3") break;  //  выход из программы
+		}
+		if (contProcNumber == "3") break;      //  выход из программы
+	}
+	return 0;
+}
---
+++ SC.cpp
@@ -0,0 +1,134 @@
+#include "SC.h"
+#include<iostream>
+
+void userReg(string& curName, string& curPassWord)   // функция для ввода данных пользователя при регистрации или входе
+{
+
+	cout << "Введите имя: ";
+	getline(cin, curName);
+	cout << "Введите пароль: ";
+	getline(cin, curPassWord);
+}
+
+PersMessage::PersMessage() // конструктор по умолчанию
+{
+	this->persName = "persName";        
+	this->persDirWord = "persDirWord";  
+	this->textInfo = "textInfo";        
+}
+string PersMessage::getCharValue(int fieldNumber) const // геттер для вывода полей класса типа string
+{
+	switch (fieldNumber)
+	{
+	case 1: return this->persName; break;
+	case 2: return this->persDirWord; break;
+	case 3: return this->textInfo;
+	}
+}
+void PersMessage::setValue(int numValue, string value) // сеттер для инициализации полей типа string
+{
+	(numValue == 1 ? this->persName = value : this->persDirWord = value);
+}
+void PersMessage::initPersMessage(string persName)  //  функция создания сообщения
+{
+	string _persDirWord;            
+	string _textInfo;               
+	this->persName = persName;      
+	cout << "Кому отправить сообщение (введите имя пользователя или 'всем'): ";
+	getline(cin, _persDirWord);
+	this->persDirWord = _persDirWord;
+	cout << "Введите текст сообщения: ";
+	getline(cin, _textInfo);
+	this->textInfo = _textInfo;
+}
+void Chat::entrChat(bool& persPresence, string& curName, string& curPassWord)      // войти в чат
+{
+	if (persArray.size())  // если массив пользователей не равен нулю
+	{
+		userReg(curName, curPassWord);   // ввод данных пользователя при входе
+		map <string, string>::iterator it = persArray.find(curName);
+		if (it != persArray.end() && it->second == curPassWord) persPresence = true;  // успешный вход пользователя
+		if (persPresence == false)
+		{
+			cout << "Пользователь с такими именем и паролем не найден. Проверьте правильность ввода данных" << endl;
+		}
+	}
+	else
+	{
+		cout << "В чате еще нет зарегистрированных пользователей." << endl;
+	}
+}
+void Chat::regChat(bool& nameInChatAlready, bool& persPresence, string& curName, string& curPassWord)    // зарегистрироваться в чате
+{
+	userReg(curName, curPassWord);                                // ввод имени и паролья пользователя при регистрации
+	map <string, string>::iterator it = persArray.find(curName);
+	if (it != persArray.end() || curPassWord == "" || curName == "") nameInChatAlready = true;  // если введенное имя уже есть или введены пустой пароль либо имя
+	if (nameInChatAlready)                                        // то регистрации не происходит
+	{
+		cout << "Пользователь с таким именем уже есть в чате либо ввод некорректен (пустое имя или пароль). Повторите ввод." << endl;
+		nameInChatAlready = false;     // возвращаем переменную проверки существования имени в чате в исходное состояние
+	}
+	else
+	{
+		persArray.insert({ curName, curPassWord });               // занесение данных пользователя в массив пользователей
+		cout << "Добро пожаловать в чат, " << curName << "!" << endl;
+		persPresence = true;
+	}
+}
+bool Chat::ifEmpty()               // если в массиве есть сообщения
+{
+	return !messArray.empty();
+}
+void Chat::showMess(string curName)              // показать сообщения
+{
+	PersMessage arrMessage;  
+	string arrName;          
+	string arrAdress;        
+	string arrMessContent;   
+	cout << "               ***               " << endl;  // оформление сообщений для лучшего восприятия информации
+	for (int i = 0; i < messArray.size(); ++i)            //  вывод на экран сообщений для конкретного пользователя
+	{
+		arrMessage = messArray[i];                        //  определение текущего сообщения
+		arrName = arrMessage.getCharValue(1);             //  определение имени владельца сообщения
+		arrAdress = arrMessage.getCharValue(2);           //  определение имени адресата сообщения
+		arrMessContent = arrMessage.getCharValue(3);      //  определение содержания сообщения
+		if (arrName == curName || arrAdress == curName || arrAdress == "всем")  //  если сообщение имеет отношение к текущему пользователю
+		{
+			cout << "Сообщение #" << i << " " << arrName << " пишет " << arrAdress << ":" << endl;  // то оно выводится на консоль
+			cout << arrMessContent << endl;
+
+		}
+	}
+	cout << "               ***               " << endl;   
+}
+void Chat::delMess()                 // удалить сообщение
+{
+	string inputMessNumber = "";
+	int arrNumber;                //  номер удаляемого сообщения
+	while (inputMessNumber == "") //  защита от некорректного ввода
+	{
+		cout << "Введите номер сообщения, которое хотите удалить: ";
+		getline(cin, inputMessNumber);                  //  ввод номера сообщения, которое необходимо удалить
+	}
+	arrNumber = stoi(inputMessNumber);                  //  преобразование номера сообщения из формата string в формат int;
+	if (arrNumber > -1 && arrNumber < messArray.size()) //  если введенный номер сообщения в границах массива сообщений
+	{
+		vector<PersMessage>::iterator it = messArray.begin() + arrNumber;
+		messArray.erase(it);      //  сообщение удаляется
+		cout << "Сообщение удалено!" << endl;
+	}
+	else cout << "Сообщение с таким номером не найдено!" << endl;          //  иначе выводится сообщение об ошибке
+}
+void Chat::sendMess(string curName)             // отправить сообщение
+{
+	PersMessage curMessage;       //  текущее сообщение
+	cout << "Пользователи чата, доступные для общения:" << endl << "<всем> ";
+
+	for (map<string, string>::iterator it = persArray.begin(); it != persArray.end(); ++it)
+	{
+		cout << "<" << it->first << "> ";      //  вывод имен других пользователей
+	}
+	cout << endl;                              //  переход на новую строку для лучшей читаемости информации
+	curMessage.initPersMessage(curName);       //  формирование сообщения
+	messArray.push_back(curMessage);
+}
